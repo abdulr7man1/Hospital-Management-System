f32047d8c8ea89ce1d56480b6a8138a95fccedfc
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-19 10:01:29 +0400

    TASK 1.1

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -3,6 +3,16 @@ package entities;
 import interfaces.Displayable;
 
 public class Person implements Displayable {
-
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String nationalId;
+    private int age;
+    private boolean activeStatus;
 
 }
