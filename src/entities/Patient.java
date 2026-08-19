fe6b001513bf3de8b22fd7ad84969f99bf7be375
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-19 12:20:54 +0400

    TASK 1.2

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -1,9 +1,16 @@
 package entities;
 
 public class Patient extends Person {
+    private String bloodGroup;
+    private String emergencyContact;
+    private String registrationDate;
+    private List<String> allergies;
+    private List<String> pastMedicalRecordIds;
+    private double outstandingBalance;
+    private boolean insured;
 
-
-    public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber, String email, String address, String nationalId, int age, boolean activeStatus) {
+    public Patient(String id, String firstName, String lastName, String dateOfBirth, String gender, String phoneNumber,
+                   String email, String address, String nationalId, int age, boolean activeStatus) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalId, age, activeStatus);
     }
 }
